4c12d71e842c3d9ef8bdc6bb1ea82c56690f9f70
lynnfaraday <lynnfaraday@users.noreply.github.com>
2024-11-12 20:35:15 -0500

    Added missing custom file.

---
+++ custom/custom_char_fields.rb
@@ -0,0 +1,83 @@
+module AresMUSH
+  module Profile
+    class CustomCharFields
+
+      # Gets custom fields for display in a character profile.
+      #
+      # @param [Character] char - The character being requested.
+      # @param [Character] viewer - The character viewing the profile. May be nil if someone is viewing
+      #    the profile without being logged in.
+      #
+      # @return [Hash] - A hash containing custom fields and values.
+      #    Ansi or markdown text strings must be formatted for display.
+      # @example
+      #    return { goals: Website.format_markdown_for_html(char.goals) }
+      def self.get_fields_for_viewing(char, viewer)
+        return { ledger: Ledger.build_ledger_view(viewer, char) }
+      end
+
+      # Gets custom fields for the character profile editor.
+      #
+      # @param [Character] char - The character being requested.
+      # @param [Character] viewer - The character editing the profile.
+      #
+      # @return [Hash] - A hash containing custom fields and values.
+      #    Multi-line text strings must be formatted for editing.
+      # @example
+      #    return { goals: Website.format_input_for_html(char.goals) }
+      def self.get_fields_for_editing(char, viewer)
+        {}
+      end
+
+      # Gets custom fields for character creation (chargen).
+      #
+      # @param [Character] char - The character being requested.
+      #
+      # @return [Hash] - A hash containing custom fields and values.
+      #    Multi-line text strings must be formatted for editing.
+      # @example
+      #    return { goals: Website.format_input_for_html(char.goals) }
+      def self.get_fields_for_chargen(char)
+        {}
+      end
+      
+      # Deprecated - use save_fields_from_profile_edit2 instead
+      def self.save_fields_from_profile_edit(char, char_data)
+        return []
+      end
+
+      # Saves fields from character creation (chargen).
+      #
+      # @param [Character] char - The character being updated.
+      # @param [Hash] chargen_data - A hash of character fields and values. Your custom fields
+      #    will be in chargen_data[:custom]. Multi-line text strings should be formatted for MUSH.
+      #
+      # @return [Array] - A list of error messages. Return an empty array ([]) if there are no errors.
+      # @example
+      #        char.update(goals: Website.format_input_for_mush(chargen_data[:custom][:goals]))
+      #        return []
+      def self.save_fields_from_chargen(char, chargen_data)
+        return []
+      end
+      
+      # Saves fields from profile editing.
+      #
+      # @param [Character] char - The character being updated.
+      # @param [Character] enactor - The character triggering the update.
+      # @param [Hash] char_data - A hash of character fields and values. Your custom fields
+      #    will be in char_data[:custom]. Multi-line text strings should be formatted for MUSH.
+      #
+      # @return [Array] - A list of error messages. Return an empty array ([]) if there are no errors.
+      # @example
+      #        char.update(goals: Website.format_input_for_mush(char_data[:custom][:goals]))
+      #        return []
+      def self.save_fields_from_profile_edit2(char, enactor, char_data)
+        # By default, this calls the old method for backwards compatibility. The old one didn't
+        # use enactor. Replace this with your own code.
+        return CustomCharFields.save_fields_from_profile_edit(char, char_data)
+      end
+
+      
+    end
+  end
+end
